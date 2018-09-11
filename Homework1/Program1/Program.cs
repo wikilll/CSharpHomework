5c7727dec185fff3430bfa0022dceaabc0066c25
wikilll <1421242102@qq.com>
2018-09-11 13:01:51 +0800

    Homework1

---
+++ Homework1/Program1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "";
+            double d1 = 0;
+            Console.Write("Please input a number : ");
+            s1 = Console.ReadLine();
+            d1 = Double.Parse(s1);
+            string s2 = "";
+            double d2 = 0;
+            Console.Write("Please input a number : ");
+            s2 = Console.ReadLine();
+            d2 = Double.Parse(s2);
+            Console.WriteLine("The product is : " + (d1 * d2));
+        }
+    }
+}
---
+++ Homework1/Program2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            string s2 = textBox2.Text;
+            double d1 = Double.Parse(s1);
+            double d2 = Double.Parse(s2);
+            double pro = d1 * d2;
+            string s3 = pro.ToString();
+            label1.Text = "The product of " + s1 + " and " + s2 + " is " + s3 + ".";
+        }
+    }
+}
